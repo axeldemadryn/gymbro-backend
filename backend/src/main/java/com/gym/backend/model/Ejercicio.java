d1a1663df744d3ae507e7acd476fedd35ad88ada
GabrielSorrentino <sorrentinogr@gmail.com>
2025-09-10 00:35:07 -0300

    Carpeta 'model' con clases Maquina y Ejercicio

---
+++ backend/src/main/java/com/gym/backend/model/Ejercicio.java
@@ -0,0 +1,26 @@
+package com.gym.backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Ejercicio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String descripcion;
+}
