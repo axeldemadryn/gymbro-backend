1b9da6971adbbbe38bdd4b6deed22743d6481d0b
Axel Herrera <axeldemadryn@hotmail.com>
2025-09-10 22:38:32 -0300

    Reubicado el paquete model

--- backend/src/main/java/com/gym/backend/model/Maquina.java
+++ backend/src/main/java/com/gym/backend/model/Maquina.java
@@ -1,10 +1,15 @@
-package com.gym.model;
+package com.gym.backend.model;
 
+import java.util.Collection;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,7 +18,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class Ejercicio {
+public class Maquina {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
@@ -23,4 +28,8 @@ public class Ejercicio {
 
     @Column(nullable = false)
     private String descripcion;
+
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "maquina_id", nullable = true)
+    private Collection<Ejercicio> ejercicios;
 }
