266c4921b8b5bb81059769038558a79ff52cdd31
GabrielSorrentino <sorrentinogr@gmail.com>
2025-10-10 01:42:01 -0300

    Staging de musculos

---
+++ staging/musculos.sql
@@ -0,0 +1,59 @@
+-- Músculos sin asociación, en principio, a un ejercicio o máquina
+INSERT INTO musculos(nombre) VALUES
+
+-- 🟩 PECHO
+('Pectoral mayor'),
+('Pectoral menor'),
+
+-- 🟦 ESPALDA
+('Dorsal ancho'),
+('Romboides'),
+('Trapecio superior'),
+('Trapecio medio'),
+('Trapecio inferior'),
+('Erectores espinales (lumbar)'),
+('Redondo mayor'),
+('Redondo menor'),
+
+-- 🟨 HOMBROS
+('Deltoides anterior'),
+('Deltoides lateral'),
+('Deltoides posterior'),
+('Serrato anterior'),
+
+-- 🟧 BRAZOS
+('Bíceps braquial'),
+('Braquial anterior'),
+('Tríceps braquial'),
+('Ancóneo'),
+('Antebrazo (flexores y extensores)'),
+
+-- 🟥 PIERNAS (tren inferior)
+('Cuádriceps (recto femoral)'),
+('Vasto medial'),
+('Vasto lateral'),
+('Vasto intermedio'),
+('Isquiotibiales (bíceps femoral)'),
+('Semitendinoso'),
+('Semimembranoso'),
+('Glúteo mayor'),
+('Glúteo medio'),
+('Glúteo menor'),
+('Aductores (grupo)'),
+('Abductores (grupo)'),
+('Sartorio'),
+('Tensor de la fascia lata'),
+('Gemelos (gastrocnemios)'),
+('Sóleo'),
+
+-- 🟪 CORE / ABDOMEN
+('Recto abdominal'),
+('Oblicuo externo'),
+('Oblicuo interno'),
+('Transverso del abdomen'),
+
+-- ⚫ MULTIFUNCIONALES / COMPUESTOS
+('Músculos de tracción (general)'),
+('Músculos de empuje (general)'),
+('Estabilizadores del core'),
+('Músculos posturales');
