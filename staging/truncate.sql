e7ff16d2758ec278b571d6e8eb5f6eaea77ce625
GabrielSorrentino <sorrentinogr@gmail.com>
2025-11-03 00:22:40 -0300

    Modificado contenido de tarjeta de staging

--- staging/truncate.sql
+++ staging/truncate.sql
@@ -1,13 +1,14 @@
 TRUNCATE TABLE
-    session_exercises,      -- Tabla intermedia entre sesiones y ejercicios
-    routine_days,           -- Tabla de días de rutina
-    weekly_routines,        -- Tabla de rutinas semanales
-    sessions,               -- Tabla de sesiones, ya que routine_days depende de ésta
-    ejercicio_maquinas,     -- Tabla intermedia entre maquinas y ejercicios
-    ejercicio_musculos,     -- Tabla intermedia entre ejercicios y musculos
-    maquina_musculos,       -- Tabla intermedia entre maquinas y musculos
-    maquinas,               -- Tabla de maquinas
-    ejercicios,             -- Tabla de ejercicios
-    musculos,               -- Tabla de musculos, ya que no depende de otras
-    users                   -- Tabla de usuarios, ya que tampoco depende de otras
+    historial_reconocimientos,          -- Tabla de historiales de reconocimiento
+    session_exercises,                  -- Tabla intermedia entre sesiones y ejercicios
+    routine_days,                       -- Tabla de días de rutina
+    weekly_routines,                    -- Tabla de rutinas semanales
+    sessions,                           -- Tabla de sesiones, ya que routine_days depende de ésta
+    ejercicio_maquinas,                 -- Tabla intermedia entre maquinas y ejercicios
+    ejercicio_musculos,                 -- Tabla intermedia entre ejercicios y musculos
+    maquina_musculos,                   -- Tabla intermedia entre maquinas y musculos
+    maquinas,                           -- Tabla de maquinas
+    ejercicios,                         -- Tabla de ejercicios
+    musculos,                           -- Tabla de musculos, ya que no depende de otras
+    users                               -- Tabla de usuarios, ya que tampoco depende de otras
 RESTART IDENTITY CASCADE;
