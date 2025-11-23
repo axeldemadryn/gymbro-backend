75ecf1a8d608529e79892073d5a412e4c3c3dc35
Axel Herrera <axeldemadryn@hotmail.com>
2025-11-23 17:51:17 -0300

    agregado endpoint para obtener plan activo del usuario, y agregado consulta en SessionExerciseRepository para idea futura de obtener los músculos que coinciden con una determinada máquina

--- backend/src/main/java/com/gym/backend/presenter/PlanPresenter.java
+++ backend/src/main/java/com/gym/backend/presenter/PlanPresenter.java
@@ -3,6 +3,7 @@ package com.gym.backend.presenter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.gym.backend.business.services.UserService;
 import com.gym.backend.model.TipoPlan;
 import com.gym.backend.model.User;
 import com.gym.backend.model.UserPlan;
+import com.gym.backend.response.Response;
 
 @RestController
 @RequestMapping("/api/planes")
@@ -32,9 +34,24 @@ public class PlanPresenter {
 
         try {
             UserPlan nuevo = userPlanService.activatePlan(user.getId(), tipoPlan);
-            return ResponseEntity.ok(nuevo);
+            return Response.ok(nuevo);
         } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            return Response.error(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @GetMapping("/mi-plan-activo")
+    public ResponseEntity<Object> obtenerPlanActivo() {
+
+        User user = userService.getAuthenticatedUser();
+        if (user == null)
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuario no autenticado");
+
+        try {
+            UserPlan plan = userPlanService.getActivePlan(user.getId());
+            return Response.ok(plan);
+        } catch (RuntimeException e) {
+            return Response.error(HttpStatus.BAD_REQUEST, e.getMessage());
         }
     }
 
